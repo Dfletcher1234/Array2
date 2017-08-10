1cd5f90853b5d48adec866bc63c2313d8e1736e4
David FletcherE <dtsfletcher@gmail.com>
2017-08-10 17:30:08 -0400

    done 2

--- exercise.rb
+++ exercise.rb
@@ -8,11 +8,23 @@ definition = {dog: "animal", pop: "drink", hotdog: "food"}
 movies = {wiz: 1939, sister: 1991, aladdin: 1992}
 cities = {toronto: 3000, van: 2000, mtl: 1000 }
 friends = {jon: 30, mary: 20, joseph: 10}
+#
+# puts coin
+# puts fcolours[1]
+# puts age.sort
+#
+# age << 0
+#
+# p movies[:wiz]
+#
+# p fcolours[2]
+#
+# cities[:reg] = 10
+#
+# coin.reverse
+#
+# p cities[:toronto]
 
-puts coin
-puts fcolours[1]
-puts age.sort
-
-age << 0
-
-p movies[:wiz]
+puts "I think #{artists[0]} are great"
+puts "I think #{artists[1]} are great"
+puts "I think #{artists[2]} are great"
