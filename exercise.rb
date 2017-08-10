f84d14e9bbf8a16c6a094322af35b3c37aa72a56
David FletcherE <dtsfletcher@gmail.com>
2017-08-10 17:10:25 -0400

    ex 1 done

---
+++ exercise.rb
@@ -0,0 +1,18 @@
+fcolours = ["green", "purple", "blue"]
+age = [33, 23, 13]
+coin = ["heads", "heads", "heads", "heads", "tails"]
+artists = ["alanis", "shania", "celine",]
+facolours = [:green, :purple, :blue]
+
+definition = {dog: "animal", pop: "drink", hotdog: "food"}
+movies = {wiz: 1939, sister: 1991, aladdin: 1992}
+cities = {toronto: 3000, van: 2000, mtl: 1000 }
+friends = {jon: 30, mary: 20, joseph: 10}
+
+puts coin
+puts fcolours[1]
+puts age.sort
+
+age << 0
+
+p movies[:wiz]
